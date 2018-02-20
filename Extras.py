9392d4bba5670fc77f5218ea5653cb25959b8c01
cmparlettpelleriti <parlett@chapman.edu>
2018-02-20 14:02:32 -0800

    starting simple

--- Extras.py
+++ Extras.py
@@ -46,3 +46,102 @@ def combineCandD(m1,m2):
 # b = simDiscrete([[0.5,0.5],[0.1,0.9]], [[0,1],[2,4]], n = 10)
 # combineCandD(a,b)
 #------- Distribution Generation --------------------
+
+#QUESTIONS
+# 1) Do we want to simulate two multivariate sets, one for 0 and one for 1?
+# OR do we want it all to be the same mv dist and let the model decide?
+# 2) AUC values
+# 3) We want to know what the difference is between
+# label shuffling with non model generated and model generated data...
+
+'''The purpose of this script is to
+
+1) Generate either related or random outputs from random data
+(both continuous and discrete data --> discrete outputs)
+
+2) Use label switching/bootstrapping to generate distributions
+of theoretical values for AUC'''
+
+
+############ Functions ##################
+class SDO(object):
+    #shuffalable data object
+    '''cov: can be  one or two dimensional'''
+    def __init__(self, n = 10, cols = [], mus = [],cov = [], props = [], opts = []):
+        print("STARTING")
+        self.n = n
+        self.data = None
+        self.Cdata = None
+        self.Ddata = None
+        self.cols = cols
+        self.mus = mus
+        self.cov = cov
+        self.props = props
+        self.opts = opts
+        if mus != []:
+            # self.simContinuous()
+            self.mvSimCont()
+            #need to account for multiple groups?
+        if props != []:
+            self.simDiscrete()
+        if type(self.Cdata) == type(self.Ddata):
+            self.combineCandD()
+        if self.cols == []:
+            self.cols = []
+            for i in range(0,len(mus)):
+                self.cols.append("c" + str(i))
+        else:
+            self.cols = cols
+        #--------------------
+    def simContinuous(self):
+        '''Simulates continuous data columns, these
+        aren't assumed to be correlated...should I add that? Maybe. IDK.'''
+        self.mus = np.array(self.mus)
+        self.cov = np.matrix(self.cov)
+        allNs = self.cov * np.random.randn(len(self.mus),self.n)
+        for ns in range(0,len(allNs)):
+            allNs[ns] += self.mus[ns]
+        self.Cdata = np.matrix(allNs)
+        self.Cdata = self.Cdata.T
+    def simDiscrete(self):
+        '''simulates discrete data columns, assumed not to be related'''
+        ns = []
+        for i in range(0,len(self.props)):
+            col = np.random.choice(a = self.opts[i], size = self.n, p = self.props[i])
+            ns.append(col)
+        n = np.matrix(ns)
+        n = n.transpose()
+        self.Ddata = n
+    def combineCandD(self):
+        '''combine continuous and discrete variables when necessary'''
+        self.data = np.concatenate((self.Cdata, self.Ddata), axis=1)
+    def labelFlip(self,flipCol):
+        '''Randomly Shuffles Classifications'''
+        flippable = list(self.data[colName])
+        random.shuffle(flippable)
+        flippable = pd.Series(flippable)
+        self.data[colName] = flippable.values
+    def mvSimCont(self):
+        '''Simulating multivariate continuous continuous data
+        '''
+        self.Cdata = np.random.multivariate_normal(np.array(self.mus),np.matrix(self.cov),size = self.n)
+        pass
+    def mvSimDisc(self,thresholds = None):
+        pass
+        # d = np.random.multivariate_normal(self.mus, self.cov, size = n)
+        # d = d.transpose()
+        # print(d)
+        # for i in d:
+    def RandomLabels(self):
+        choices = []
+        for i in range(0,len(opts)):
+            pass
+
+###############TEST##############################
+
+mySDO = SDO(n = 100,mus = [10,10,15], cov = [[1,.5,.45],[.5,1,.45],[.45,.5,1]])
+print(mySDO.Cdata)
+
+#b = ms.logistic_regression(mySDO.Cdata)
+# b = ms.multi_layer_perceptron(mySDO.Cdata)
+# print(b)
